6a46882f91795b5043d929a4657711881d1c10be
DemocraticDictator <soccerscoutc@gmail.com>
2020-10-16 23:21:02 -0700

    Added Character Counter

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -20,11 +23,13 @@ import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
 
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 280;
     public static final String TAG ="ComposeActivity";
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvCharCounter;
+    String charCounterText;
 
     TwitterClient client;
 
@@ -37,6 +42,34 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCharCounter = findViewById(R.id.tvCharCounter);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            int charCounter = 0;
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                if (before > count)
+                    charCounter--;
+                else
+                    charCounter++;
+                if (charCounter > MAX_TWEET_LENGTH)
+                    charCounterText = charCounter + "/" + MAX_TWEET_LENGTH + "  TOO LONG!";
+                else
+                    charCounterText = charCounter + "/" + MAX_TWEET_LENGTH;
+                tvCharCounter.setText(charCounterText);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+            }
+        });
 
         btnTweet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,12 +80,12 @@ public class ComposeActivity extends AppCompatActivity {
                     return;
                 }
                 if (tweetContent.length() > MAX_TWEET_LENGTH) {
-                    Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_LONG).show();
-                    return;
-                }
-                Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                        Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
 
-                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
 
                     @Override
                     public void onSuccess(int statusCode, Headers headers, JSON json) {
